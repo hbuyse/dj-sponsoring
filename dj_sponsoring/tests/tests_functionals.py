43740e68eed8be58ba0efff72b5f9adfbd3431b1
Henri Buyse <henri.buyse@gmail.com>
2018-08-09 09:18:34 +0200

    Add functional testing
    Do not forget to start the example runserver to do functional testing

---
+++ dj_sponsoring/tests/tests_functionals.py
@@ -0,0 +1,52 @@
+# coding=utf-8
+
+"""."""
+
+import unittest
+import time
+
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+
+
+unittest.TestLoader.sortTestMethodsUsing = None
+
+
+class NewVisitorTest(unittest.TestCase):
+    """."""
+
+    def setUp(self):
+        """Setup."""
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        """Teardown."""
+        self.browser.quit()
+
+    def test_django_sponsoring_title(self):
+        """Test the title in the dj-sponsoring."""
+        self.browser.get('http://localhost:8000/sponsors')
+        self.assertIn('dj-sponsoring', self.browser.title)
+
+    def test_no_sponsor_in_sponsor_list(self):
+        """."""
+        # Edith has heard about a cool new online to-do app. She goes
+        # to check out its homepage
+        self.browser.get('http://localhost:8000/sponsors')
+
+        # She notices the page title and header mention to-do lists
+        self.assertIn("No sponsors", self.browser.page_source)
+
+    def test_create_sponsor(self):
+        """."""
+        self.browser.get('http://localhost:8000/sponsors/create')
+
+        print(self.browser.page_source)
+        select = Select(self.browser.find_element_by_id('id_name'))
+        select.send_keys("Any sponsor")
+
+        time.sleep(2)
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
