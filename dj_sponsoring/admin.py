94fe1ad1a3de8634b19cbc8ca5505de561b03ff8
Henri Buyse <henri.buyse@gmail.com>
2018-08-28 17:33:53 +0200

    Add admin views

---
+++ dj_sponsoring/admin.py
@@ -0,0 +1,42 @@
+"""..."""
+
+from django.contrib import admin
+
+from .models import (
+    Sponsor,
+    SponsorFile,
+    SponsorImage,
+)
+
+
+@admin.register(Sponsor)
+class SponsorAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'url',
+        'created',
+        'modified',
+    )
+    date_hierarchy = 'created'
+
+
+@admin.register(SponsorFile)
+class SponsorFileAdmin(admin.ModelAdmin):
+    list_display = (
+        'sponsor',
+        'name',
+        'created',
+        'modified',
+    )
+    date_hierarchy = 'created'
+
+
+@admin.register(SponsorImage)
+class SponsorImageAdmin(admin.ModelAdmin):
+    list_display = (
+        'sponsor',
+        'alt',
+        'created',
+        'modified',
+    )
+    date_hierarchy = 'created'
